7b56fc7f163fa229b2cd6f7997f5576700899890
CarlosGuerrero9909 <caraguerrerob@correo.udistrital.edu.co>
2023-01-05 11:50:16 -0500

    finished part 3

--- part3/phoneBackend/index.js
+++ part3/phoneBackend/index.js
@@ -1,95 +1,118 @@
+/* ------------------------- Cabecera --------------------------------------*/
 const express = require('express')
-const morgan = require('morgan')
-
-//nuestro propido token 'body' que retorna el body de la peticion en formato json
-morgan.token('body', request => JSON.stringify(request.body))
 
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const morgan = require('morgan')
+const Person = require('./models/person')
+
+// Middleware de nivel de solicitud HTTP permite registrar las solicitudes junto con alguna otra información dependiendo de su configuración
+morgan.token('body', (request) => JSON.stringify(request.body)) // nuestro propido token 'body' que retorna el body de la peticion en formato json
 
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms')) //usa dicha configuracion de manera global para todas las peticiones
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms'),
+) // usa dicha configuracion de manera global para todas las peticiones
+app.use(cors())
+app.use(express.static('build')) // busca y devuelve los archivos estáticos solicitados por el usuario y recibe como parámetro la ruta del folder donde están esos archivos.
 
-let persons = [
-	{
-		name: "Arto Hellas",
-		number: "3230923032",
-		id: 1
-	},
-	{
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-		id: 2
-	},
-	{
-		name: "Dan Abramov",
-		number: "12-43-234345",
-		id: 3
-	},
-	{
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-		id: 4
-	}
-]
+/* ------------------- Rutas (peticiones)  Endpoints --------------------*/
 
-app.get('/', (request, response) => {
-	response.send('<h1>Hello World!</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-	response.json(persons)
+app.post(
+  '/api/persons',
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
+  (request, response, next) => {
+    const { body } = request
+
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
+
+    person
+      .save()
+      .then((savedPerson) => savedPerson.toJSON())
+      .then((savedAndFormattedPerson) => {
+        response.json(savedAndFormattedPerson)
+      })
+      .catch((error) => next(error))
+  },
+)
+
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then((people) => {
+      response.json(people)
+    })
+    .catch((error) => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      person ? response.json(person) : response.status(404).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { body } = request
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true }) // Agregamos el parámetro opcional { new: true }, que hará que nuestro controlador de eventos sea llamado con el nuevo documento modificado en lugar del original.
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/info', (request, response) => {
-	response.send(`
-		Phonebook has info for ${persons.length} people <br></br>
+  response.send(`
+		Phonebook has info for ${Person.estimatedDocumentCount()} people <br></br>
 		${new Date()}
 	`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(person => person.id === id)
-	if (person)
-		response.json(person)
-	else
-		response.status(404).end()
-})
+/* ------------------------------ El orden de carga de los middleware-----------------------------------------------------*/
 
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
-	response.status(204).end()
-})
+// midleware despues de rutas (endpoint desconocido) peticiones a rutas inexistentes
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint) // handler of requests with unknown endpoint
 
-app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :body'), (request, response) => {
-	const body = request.body
+// middleware manejador de errores
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error) // middleware pasa el error al controlador de errores Express predeterminado.
+}
+app.use(errorHandler) // handler of requests with result to errors
 
-	if (!body.name || !body.number)
-		return response.status(400).json({ 
-      error: 'content missing' 
-    })
-
-	if(persons.find(person => person.name === body.name))
-		return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
-
-	const person = {
-		name: body.name,
-		number: body.number,
-		id: Math.floor(Math.random() * 100)
-	}
-	persons = persons.concat(person)
-	response.json(person)
-})
-
-const PORT = 3001
+/* -------------------- Puerto ------------------------------*/
+const { PORT } = process.env
 app.listen(PORT, () => {
-	console.log(`server running on port ${PORT}`)
+  console.log(`server running on port ${PORT}`)
 })
-
-
-
-
-
